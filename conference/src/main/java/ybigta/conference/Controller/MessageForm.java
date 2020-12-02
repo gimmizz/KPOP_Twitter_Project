8cf8753ff5d70ca574989ca872b5b4fa862f1951
Roddie <ew5224@naver.com>
2020-12-03 00:40:11 +0900

    자고 싶어

---
+++ conference/src/main/java/ybigta/conference/Controller/MessageForm.java
@@ -0,0 +1,19 @@
+package ybigta.conference.Controller;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.lang.reflect.Member;
+
+@Getter
+@Setter
+public class MessageForm {
+    private Integer messageid;
+    private String context;
+    private String name;
+
+
+
+}
