30803595970243dc1e75d884a446485f89cf68d0
Roddie <ew5224@naver.com>
2020-12-21 19:32:33 +0900

    Web 시작

---
+++ Twitter Web/src/main/java/ybigta/kpoptwitter/KpoptwitterApplication.java
@@ -0,0 +1,13 @@
+package ybigta.kpoptwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpoptwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpoptwitterApplication.class, args);
+	}
+
+}
